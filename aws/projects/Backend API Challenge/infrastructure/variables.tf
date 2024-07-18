d1d91bcc37b2562ad73374ca6b50d6a1b9761142
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-07-18 13:57:44 +0300

    rewriting my terraform files

--- aws/projects/Backend API Challenge/infrastructure/variables.tf
+++ aws/projects/Backend API Challenge/infrastructure/variables.tf
@@ -1,5 +1,34 @@
 variable "s3_bucket_name" {
-  type        = "type"
-  description = "description"
-  default     = "type"
+  type        = "string"
+  description = "S3 bucket name"
+  default     = "cloud-resume-bucket"
+}
+
+variable "dynamodb_table_name" {
+  type        = "string"
+  description = "DynamoDB table name"
+  default     = "resume-table"
+}
+
+variable "aws_account_id" {
+    type        = "string"
+    description = "AWS account ID"
+    default     = "478056848832"
+ }
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "lambda_function_name" {
+  type        = "string"
+  description = "Lambda function name"
+  default     = "resume-builder"
+}
+
+variable "api_gateway_name" {
+  type        = "string"
+  default     = "resume-builder"
 }
